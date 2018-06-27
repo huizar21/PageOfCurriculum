403c27435e03480ce0809289518fd2e5fa86ea0b
huizar21 <jacintohc12@gmail.com>
2018-06-27 15:10:46 -0500

    Before that less

---
+++ js/Functions.js
@@ -0,0 +1,267 @@
+function gnral(){
+    document.getElementById("001").style.display = 'none';
+    document.getElementById("002").style.display = 'none';
+    document.getElementById("003").style.display = 'none';
+    document.getElementById("004").style.display = 'none';
+    document.getElementById("005").style.display = 'none';
+    document.getElementById("006").style.display = 'none';
+    document.getElementById("007").style.display = 'none';
+    document.getElementById("008").style.display = 'none';
+    document.getElementById("009").style.display = 'none';
+    document.getElementById("010").style.display = 'none';
+    document.getElementById("011").style.display = 'none';
+}
+function mostrarfoto() {
+    gnral();
+    document.getElementById("001").style.display = 'block';
+    document.getElementById("002").style.display = 'block';
+}
+function inputcontdat(){
+    gnral();
+    document.getElementById("003").style.display = 'block';
+}
+function inputstud(){
+    gnral();
+    document.getElementById("004").style.display = 'block';
+}
+
+function inputhab(){
+    gnral();
+    document.getElementById("005").style.display = 'block';
+}
+function inputexp(){
+    gnral();
+    document.getElementById("006").style.display = 'block';
+}
+function inputpro(){
+    gnral();
+    document.getElementById("007").style.display = 'block';
+}
+function inputint(){
+    gnral();
+    document.getElementById("008").style.display = 'block';
+}
+function inputsoft(){
+    gnral();
+    document.getElementById("009").style.display = 'block';
+}
+function inputidi(){
+    gnral();
+    document.getElementById("010").style.display = 'block';
+}
+function inputref(){
+    gnral();
+    document.getElementById("011").style.display = 'block';
+}
+function normal1(){
+    document.getElementById("b1").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b1").style.borderStyle ="double";
+    document.getElementById("b1").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b1").style.display = "flex"; 
+    document.getElementById("b1").style.alignItems = "center";
+    document.getElementById("b1").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b1").style.fontSize = "110%";
+    document.getElementById("b1").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b1").style.textAlign ="center";
+}
+function animation1(){
+    document.getElementById("b1").style.backgroundColor ="black";
+    document.getElementById("b1").style.color = "white";
+    document.getElementById("b1").style.borderColor = "white";
+    document.getElementById("b1").style.fontSize= "150%";
+   
+}
+function normal2(){
+    document.getElementById("b2").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b2").style.borderStyle ="double";
+    document.getElementById("b2").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b2").style.display = "flex"; 
+    document.getElementById("b2").style.alignItems = "center";
+    document.getElementById("b2").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b2").style.fontSize = "110%";
+    document.getElementById("b2").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b2").style.textAlign ="center";
+}
+function animation2(){
+    document.getElementById("b2").style.backgroundColor ="black";
+    document.getElementById("b2").style.color = "white";
+    document.getElementById("b2").style.borderColor = "white";
+    document.getElementById("b2").style.fontSize= "150%";
+}
+function normal3(){
+    document.getElementById("b3").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b3").style.borderStyle ="double";
+    document.getElementById("b3").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b3").style.display = "flex"; 
+    document.getElementById("b3").style.alignItems = "center";
+    document.getElementById("b3").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b3").style.fontSize = "110%";
+    document.getElementById("b3").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b3").style.textAlign ="center";
+}
+function animation3(){
+    document.getElementById("b3").style.backgroundColor ="black";
+    document.getElementById("b3").style.color = "white";
+    document.getElementById("b3").style.borderColor = "white";
+    document.getElementById("b3").style.fontSize= "180%";
+}
+function normal4(){
+    document.getElementById("b4").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b4").style.borderStyle ="double";
+    document.getElementById("b4").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b4").style.display = "flex"; 
+    document.getElementById("b4").style.alignItems = "center";
+    document.getElementById("b4").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b4").style.fontSize = "110%";
+    document.getElementById("b4").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b4").style.textAlign ="center";
+}
+function animation4(){
+    document.getElementById("b4").style.backgroundColor ="black";
+    document.getElementById("b4").style.color = "white";
+    document.getElementById("b4").style.borderColor = "white";
+    document.getElementById("b4").style.fontSize= "150%";
+}
+function normal5(){
+    document.getElementById("b5").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b5").style.borderStyle ="double";
+    document.getElementById("b5").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b5").style.display = "flex"; 
+    document.getElementById("b5").style.alignItems = "center";
+    document.getElementById("b5").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b5").style.fontSize = "110%";
+    document.getElementById("b5").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b5").style.textAlign ="center";
+}
+function animation5(){
+    document.getElementById("b5").style.backgroundColor ="black";
+    document.getElementById("b5").style.color = "white";
+    document.getElementById("b5").style.borderColor = "white";
+    document.getElementById("b5").style.fontSize= "150%";
+}
+function normal6(){
+    document.getElementById("b6").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b6").style.borderStyle ="double";
+    document.getElementById("b6").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b6").style.display = "flex"; 
+    document.getElementById("b6").style.alignItems = "center";
+    document.getElementById("b6").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b6").style.fontSize = "110%";
+    document.getElementById("b6").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b6").style.textAlign ="center";
+}
+function animation6(){
+    document.getElementById("b6").style.backgroundColor ="black";
+    document.getElementById("b6").style.color = "white";
+    document.getElementById("b6").style.borderColor = "white";
+
+}
+function normal7(){
+    document.getElementById("b7").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b7").style.borderStyle ="double";
+    document.getElementById("b7").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b7").style.display = "flex"; 
+    document.getElementById("b7").style.alignItems = "center";
+    document.getElementById("b7").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b7").style.fontSize = "110%";
+    document.getElementById("b7").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b7").style.textAlign ="center";
+}
+function animation7(){
+    document.getElementById("b7").style.backgroundColor ="black";
+    document.getElementById("b7").style.color = "white";
+    document.getElementById("b7").style.borderColor = "white";
+    document.getElementById("b7").style.fontSize= "150%";
+}
+function normal8(){
+    document.getElementById("b8").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b8").style.borderStyle ="double";
+    document.getElementById("b8").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b8").style.display = "flex"; 
+    document.getElementById("b8").style.alignItems = "center";
+    document.getElementById("b8").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b8").style.fontSize = "110%";
+    document.getElementById("b8").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b8").style.textAlign ="center";
+}
+function animation8(){
+    document.getElementById("b8").style.backgroundColor ="black";
+    document.getElementById("b8").style.color = "white";
+    document.getElementById("b8").style.borderColor = "white";
+    document.getElementById("b8").style.fontSize= "150%";
+}
+function normal9(){
+    document.getElementById("b9").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b9").style.borderStyle ="double";
+    document.getElementById("b9").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b9").style.display = "flex"; 
+    document.getElementById("b9").style.alignItems = "center";
+    document.getElementById("b9").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b9").style.fontSize = "110%";
+    document.getElementById("b9").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b9").style.textAlign ="center";
+}
+function animation9(){
+    document.getElementById("b9").style.backgroundColor ="black";
+    document.getElementById("b9").style.color = "white";
+    document.getElementById("b9").style.borderColor = "white";
+    document.getElementById("b9").style.fontSize= "150%";
+}
+function normal10(){
+    document.getElementById("b10").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b10").style.borderStyle ="double";
+    document.getElementById("b10").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b10").style.display = "flex"; 
+    document.getElementById("b10").style.alignItems = "center";
+    document.getElementById("b10").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b10").style.fontSize = "110%";
+    document.getElementById("b10").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b10").style.textAlign ="center";
+}
+function animation10(){
+    document.getElementById("b10").style.backgroundColor ="black";
+    document.getElementById("b10").style.color = "white";
+    document.getElementById("b10").style.borderColor = "white";
+    document.getElementById("b10").style.fontSize= "150%";
+}
+function normal11(){
+    document.getElementById("b11").style.backgroundColor = "rgb(94, 100, 100)";
+    document.getElementById("b11").style.borderStyle ="double";
+    document.getElementById("b11").style.borderRadius = "rgb(255, 255, 255)";
+    document.getElementById("b11").style.display = "flex"; 
+    document.getElementById("b11").style.alignItems = "center";
+    document.getElementById("b11").style.fontFamily = "Cabin Sketch";
+    document.getElementById("b11").style.fontSize = "110%";
+    document.getElementById("b11").style.color ="rgb(255, 255, 255)";
+    document.getElementById("b11").style.textAlign ="center";
+}
+function animation11(){
+    document.getElementById("b11").style.backgroundColor ="black";
+    document.getElementById("b11").style.color = "white";
+    document.getElementById("b11").style.borderColor = "white";
+    document.getElementById("b11").style.fontSize= "125%";
+}
+function imgasnor(){
+    document.getElementById("imgas01").style.marginTop = "15%";
+    document.getElementById("imgas01").style.width = "60%";
+    document.getElementById("imgas01").style.height = "20%";
+    document.getElementById("imgas01").style.display = "block";
+    document.getElementById("imgas01").style.marginLeft = "auto";
+    document.getElementById("imgas01").style.marginRight ="auto";
+    document.getElementById("imgas01").style.borderStyle = "outset";
+    document.getElementById("imgas01").style.borderWidth = "1%";
+    document.getElementById("imgas01").style.borderColor = "rgb(14, 14, 17)";
+    document.getElementById("imgas01").style.marginBottom = "4%";
+}
+function bigimage(){
+    document.getElementById("imgas01").style.marginTop = "15%";
+    document.getElementById("imgas01").style.width = "70%";
+    document.getElementById("imgas01").style.height = "25%";
+    document.getElementById("imgas01").style.display = "block";
+    document.getElementById("imgas01").style.marginLeft = "auto";
+    document.getElementById("imgas01").style.marginRight ="auto";
+    document.getElementById("imgas01").style.borderStyle = "outset";
+    document.getElementById("imgas01").style.borderWidth = "1%";
+    document.getElementById("imgas01").style.borderColor = "rgb(14, 14, 17)";
+    document.getElementById("imgas01").style.marginBottom = "4%";
+}
